9f1308ffbbdaa0cc2ebfae51490fca730062c320
Arkmorrow <steven.zheng2@outlook.com>
2022-11-15 02:38:28 +1100

    Initial Commit

---
+++ ServerMonitor/ServerMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServerMonitorApp.swift
+//  ServerMonitor
+//
+//  Created by Xiangwen Zheng on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ServerMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
